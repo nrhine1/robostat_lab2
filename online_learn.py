4d84a6053cc8f2b32f902d5c3e16ec9c8d660985
Hanzhang Hu <echo.hhz.nj@gmail.com>
2014-10-28 18:22:05 -0400

    online_learn.py

--- online_learn.py
+++ online_learn.py
@@ -1,29 +1,36 @@
 import numpy as np
 import os,sys
+import sklearn.metrics
 
 
 class online_learner(object):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def predict(x):
-    pass
+    def predict(x):
+        pass
 
-  def fit(x, y):
-    pass
+    def fit(x, y):
+        pass
 
-  def evaluate(X,Y):
-    n_samples = X.shape[0]
-    for (xi, x) in enumerate(X):
+    def evaluate(X,Y):
+        n_samples = X.shape[0]
+        Y_hat = np.zeros(Y.shape)
+        for (xi, x) in enumerate(X):
+            y_hat = predict(x)
+            Y_hat[xi] = y_hat
+        # Compute confusion mat
+        confusion_mat = sklearn.metrics.confusion_matrix(Y, Y_hat, labels=None)
+
 
 class online_logistic(online_learner):
-  def __init__(self, l2_lam):
-    pass
+    def __init__(self, l2_lam):
+        pass
 
-  def predict(x):
-    pass
+    def predict(x):
+        pass
 
-  def fit(x, y):
-    pass
+    def fit(x, y):
+        pass
 
 
