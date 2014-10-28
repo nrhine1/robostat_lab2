271cbecd8400ea45113899c10eb99a4c4dbf5951
Hanzhang Hu <echo.hhz.nj@gmail.com>
2014-10-28 16:13:40 -0400

    online learn  base file

---
+++ online_learn.py
@@ -0,0 +1,29 @@
+import numpy as np
+import os,sys
+
+
+class online_learner(object):
+  def __init__(self):
+    pass
+
+  def predict(x):
+    pass
+
+  def fit(x, y):
+    pass
+
+  def evaluate(X,Y):
+    n_samples = X.shape[0]
+    for (xi, x) in enumerate(X):
+
+class online_logistic(online_learner):
+  def __init__(self, l2_lam):
+    pass
+
+  def predict(x):
+    pass
+
+  def fit(x, y):
+    pass
+
+
