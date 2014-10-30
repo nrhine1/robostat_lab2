e836c0ee86e0a455f6e71748bc57dacf2b6d46f0
nrhine1 <nrhineha@cs.cmu.edu>
2014-10-30 12:45:54 -0400

    online svm does a thing

--- convert_data_to_numpy.py
+++ convert_data_to_numpy.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 import os, sys, argh, pdb, numpy
+import sklearn.linear_model
+
+def test_scikit_learner():
+    do = dataset_oakland(numpy_fn = 'data/oakland_part3_am_rf.node_features.npz')
+    
+    C = 1
+    learner = sklearn.linear_model.LogisticRegression(penalty = 'l2', C = C)
+
+    split_size = .8
+
+    inds = range(do.labels.shape[0])
+    train_inds = sorted(numpy.random.choice(inds,
+                                            size = int(split_size * do.labels.shape[0]),
+                                            replace = False))
+    print "here"
+    test_inds = [i for i in inds if i not in train_inds]
+    print "therer"
+
 
 class dataset_oakland(object):
     def __init__(self,  fn = 'data/oakland_part3_am_rf.node_features', numpy_fn = None):
@@ -20,12 +38,26 @@ class dataset_oakland(object):
            
         self.points = self.data[:, :3]
         self.labels = self.data[:, 4]
+
+        if self.labels.ndim == 1:
+            self.labels = self.labels[:, numpy.newaxis]
+
         self.features = self.data[:, 5:]
+
+        self.points_normalized = self.points / numpy.linalg.norm(self.points, axis = 1)[:, numpy.newaxis]
+        self.features = numpy.hstack((self.points_normalized, self.features))
+
+        self.label_map  = {'veg' : 1004, 'wire' : 1100, 'pole':  1103, 'ground' : 1200, 'facade' : 1400}
+
+        for (label_type, val) in self.label_map.items():
+            setattr(self, '{}_inds'.format(label_type), self.labels == val)
 
 def main():
     line_skip = 3
 
     do = dataset_oakland(numpy_fn = 'data/oakland_part3_am_rf.node_features.npz')
+    test_scikit_learner()
+    pdb.set_trace()
 
 if __name__ == '__main__':
     argh.dispatch_command(main)
--- online_learn.py
+++ online_learn.py
@@ -1,20 +1,44 @@
 import numpy as np
+import numpy, pdb
 import os,sys
+import matplotlib.pyplot as plt
 
+import convert_data_to_numpy as convert
 
 class online_learner(object):
   def __init__(self):
     pass
 
-  def predict(x):
+  def predict(self, x):
     pass
 
-  def fit(x, y):
+  def fit(self, x, y, t):
     pass
 
-  def evaluate(X,Y):
+  def compute_single_loss(self, y_gt, y_p):
+    pass
+
+  def evaluate(self, X, Y):
     n_samples = X.shape[0]
+
+    Y_p = numpy.zeros_like(Y)
+    losses = numpy.zeros_like(Y)
+    total_loss = 0
+
     for (xi, x) in enumerate(X):
+      if xi % 10 == 0:
+        print "round ", xi, total_loss
+
+      y_p = self.predict(x)
+      Y_p[xi] = y_p
+
+      loss = self.compute_single_loss(Y[xi], y_p)
+      total_loss += loss
+      losses[xi] = loss
+      
+      self.fit(x, Y[xi])
+
+    return Y_p, losses
 
 class online_logistic(online_learner):
   def __init__(self, l2_lam):
@@ -27,3 +51,86 @@ class online_logistic(online_learner):
     pass
 
 
+class online_svm(online_learner):
+  def __init__(self, lam, feature_size, margin = 1):
+    self.lam = float(lam)
+    self.w = numpy.zeros((feature_size,), dtype = numpy.float64)
+
+    self.t = 1
+    self.batch_size = 100
+    self.margin = margin
+    self.sqrt_lam = numpy.sqrt(self.lam)
+    
+  def predict(self, x):
+    y_p = numpy.dot(self.w.T, x)
+    return y_p
+    
+  def compute_single_loss(self, y_gt, y_p):
+    prod = y_gt * y_p
+    loss = max(0, self.margin - prod) + self.lam  * numpy.dot(self.w.T, self.w) / 2.0
+    return loss
+    
+  def fit(self, x, y_gt, do_batch = False):
+    alpha = 1.0 / (self.lam * self.t)
+    y_p = self.predict(x)
+    if y_gt * y_p < self.margin:
+      grad = - alpha * self.lam * self.w + alpha * y_gt * x
+    else:
+      grad = - alpha * self.lam * self.w
+
+    if not do_batch:
+      self.w += grad
+      
+      wtw = numpy.dot(self.w.T, self.w)
+      if wtw > 1.0 / self.lam:
+        self.w = self.w / (numpy.sqrt(wtw) * (self.sqrt_lam))
+
+    self.t += 1
+    return self.w
+
+  def evaluate(self, X, Y):
+    inds = range(Y.shape[0])
+    numpy.random.shuffle(inds)
+    
+    X = X[inds, :]
+    Y = Y[inds, :]
+
+    X_norm = X / numpy.linalg.norm(X, axis = 1)[:, numpy.newaxis]
+    return super(online_svm, self).evaluate(X, Y)
+
+def main():
+  do_svm = True
+  
+  data_o = convert.dataset_oakland(numpy_fn = 'data/oakland_part3_am_rf.node_features.npz')
+
+  lam = 1e-4
+  osvm = online_svm(lam = lam, feature_size = data_o.features.shape[1])
+  
+  
+
+  wall_and_ground_inds = numpy.where(numpy.logical_or(data_o.facade_inds, data_o.ground_inds))[0]
+
+  Y = data_o.labels[wall_and_ground_inds]
+  X = data_o.features[wall_and_ground_inds, :]
+
+  Y[Y == data_o.label_map['facade']] = -1
+  Y[Y == data_o.label_map['ground']] = 1
+
+  ypred, losses = osvm.evaluate(X, Y)
+  
+  classification = ypred > 0
+  n_right = (classification == (Y > 0)).sum()
+  accuracy = n_right / float(Y.shape[0])
+
+  cum_losses = numpy.cumsum(losses - osvm.lam * numpy.dot(osvm.w.T, osvm.w) / 2.)
+
+  print "right, accuracy: {}, {}".format(n_right, accuracy)
+  print "w: {}".format(osvm.w)
+  plt.figure()
+  plt.plot(range(losses.shape[0]), cum_losses)
+  plt.show(block = False)
+
+  pdb.set_trace()
+
+if __name__ == '__main__':
+  main()
